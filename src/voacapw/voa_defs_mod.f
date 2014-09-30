ef482a7a0df6d74fc2a6ed872d0bc3d1122db870
James Watson <jwatson@neomailbox.ch>
2014-09-30 07:45:56 +0300

    Changelog is now in a Markdown format.  Bumped version number to 0.6.5.

--- src/voacapw/voa_defs_mod.f
+++ src/voacapw/voa_defs_mod.f
@@ -1,5 +1,5 @@
       module voa_defs_mod
         integer, parameter :: MAX_AREA_MONTHS = 25
-        character(len=*), parameter :: VOACAPL_VERSION = '0.6.4'
+        character(len=*), parameter :: VOACAPL_VERSION = '0.6.5'
       end module voa_defs_mod
 
