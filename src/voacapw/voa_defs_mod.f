585d92cc280949df94999f0a5738ed1575bf97fc
James Watson <jwatson@localhost.localdomain>
2015-05-07 14:16:31 +0300

    Bumped to version 0.6.6

--- src/voacapw/voa_defs_mod.f
+++ src/voacapw/voa_defs_mod.f
@@ -1,5 +1,5 @@
       module voa_defs_mod
         integer, parameter :: MAX_AREA_MONTHS = 25
-        character(len=*), parameter :: VOACAPL_VERSION = '0.6.5'
+        character(len=*), parameter :: VOACAPL_VERSION = '0.6.6'
       end module voa_defs_mod
 
