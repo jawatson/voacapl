558a8b4541d4d35f7cfca2decd8588243c1e1303
James Watson <jwatson@neomailbox.ch>
2018-02-26 19:14:35 +0300

    Removed the old source tree.

--- src/voacapw/getkmf.f
+++
@@ -1,8 +0,0 @@
-c###getkmf.for
-      SUBROUTINE GETKMF
-C--------------------------------
-C.....MUF AS A FUNCTION OF K LINES
-C.....NOT YET DEVELOPED (METHOD 12)
-      RETURN
-      END
-C--------------------------------
