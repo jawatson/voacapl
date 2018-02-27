f661d8bd43cba5840834184f49e9b4d5c690ece6
James Watson <jimwatson@mac.com>
2018-02-27 07:37:28 +0300

    The hours are now read into the application from the idx file to hndle non-24 hour files.  Tidied up the ascii output using formatters to align the columns.

--- voacapl/itshfbc/bin/dst/dst2ascii.f90
+++ voacapl/itshfbc/bin/dst/dst2ascii.f90
@@ -1,33 +1,91 @@
 program dst2ascii
-    REAL :: gcdkm, xlat, xlon, MUF, FOT, ANGLE, DELAY, VHITE, MUFday, LOSS
-    REAL :: DBU, SDBW, NDBW, SNR, RPWRG, REL, MPROB, SPRB, SIGLW, SIGUP, SNRLW, SNRUP
-    REAL :: TGAIN, RGAIN, SNRxx, DBM
-    INTEGER, PARAMETER :: NUMDIST = 51
-    INTEGER, PARAMETER :: NUMFREQ = 8
-    INTEGER, PARAMETER :: HOURBLK = NUMDIST * NUMFREQ
-    INTEGER :: ptr, utcPtr, freqPtr
-    CHARACTER(4) :: xmode
-    print *, "Opening DST file"
-    open(20,file='voacapd.dst',status='old', form='unformatted',access='direct',recl=108)
-    open(30,file='voacapd.asc')
-    rewind(30)
-    DO utcPtr = 1, 24
-        write(30, '(AI2)') "UTC=", utcPtr
-        DO freqPtr = 1, 8
-            write(30, '(AI2)') "Freq:", freqPtr
-            write(30, '(AI2)') " id   gcdkm  Latitude Longitude Mode  MUF     FOT     ANGLE   &
-                    DELAY   VHITE   MUFday  LOSS    DBU     SDBW    NDBW    SNR     RPWRG   &
-                    REL     MPROB   SPRB    SIGLW   SIGUP   SNRLW   SNRUP   TGAIN   RGAIN   &
-                    SNRxx   DBM"
-            DO ptr = NUMDIST-1, 0, -1
-                read(20, rec=((utcPtr-1)*HOURBLK)+(ptr*8)+freqPtr ) gcdkm,xlat,xlon,xmode, MUF, &
+    logical*1 file_exists
+    real :: gcdkm, xlat, xlon, MUF, FOT, ANGLE, DELAY, VHITE, MUFday, LOSS
+    real :: DBU, SDBW, NDBW, SNR, RPWRG, REL, MPROB, SPRB, SIGLW, SIGUP, SNRLW, SNRUP
+    real :: TGAIN, RGAIN, SNRxx, DBM
+    integer :: num_args
+    character(len=128) :: itshfbc_path = ""
+    integer :: ios
+    integer :: NUMDIST, NUMFREQ, NUMHOUR
+    real, dimension(1 : 25) :: FREQS
+    integer, dimension(1 : 25) :: hours
+    integer, parameter :: IDX_FILE = 70
+    integer, parameter :: DST_FILE = 80
+    integer, parameter :: ASC_FILE = 90
+    integer :: HOURBLK
+    integer :: ptr, utcPtr, freqPtr, offset, id = 0
+    character(200) :: index_buffer
+    character(4) :: xmode
+    character(120) :: RUN_DIR
+    character(len=1), parameter :: PATH_SEPARATOR ='/'
+    character(len=128) :: idx_path, dst_path
+
+    num_args = command_argument_count()
+    
+    if (num_args == 1) then
+        call get_command_argument(1, itshfbc_path)
+        idx_path = trim(itshfbc_path)//PATH_SEPARATOR//'run'//PATH_SEPARATOR//'voacapd.idx'
+        dst_path = trim(itshfbc_path)//PATH_SEPARATOR//'run'//PATH_SEPARATOR//'voacapd.dst'
+    else
+        idx_path = 'voacapd.idx'
+        dst_path = 'voacapd.dst'
+    end if
+    
+    inquire(file=idx_path, exist=file_exists)
+    if (.not.file_exists) then
+        stop 'Unable to open IDX file '//idx_path
+    end if
+
+    inquire(file=dst_path, exist=file_exists)
+    if (.not.file_exists) then
+        stop 'Unable to open DST file '//idx_path
+    end if
+
+    open(IDX_FILE, file=idx_path, status='old')
+    ! Read the number of distances
+    read(IDX_FILE, '(I5A)') NUMDIST, index_buffer
+
+    ! Read the frequencies
+    read(IDX_FILE, '(I2A)') NUMFREQ, index_buffer
+    do freqPtr = 1, NUMFREQ
+        offset = 1 + (7*(freqPtr-1))
+        read(index_buffer((offset): (offset+7)), '(F7.3)') FREQS(freqPtr)
+    end do
+
+    ! Read the hours
+    read(IDX_FILE, '(I3A)') NUMHOUR, index_buffer
+    do ptr = 1, NUMHOUR
+        offset = 1 + (3*(ptr-1))
+        read(index_buffer((offset): (offset+3)), '(I3)') hours(ptr)
+    end do
+
+    close(IDX_FILE)
+
+    HOURBLK = NUMDIST * NUMFREQ
+
+    open(DST_FILE,file=dst_path,status='old', form='unformatted',access='direct',recl=108)
+    open(ASC_FILE,file='voacapd.asc')
+    rewind(ASC_FILE)
+    
+    do utcPtr = 1, NUMHOUR
+        write(ASC_FILE, '(AI2A)') "UTC:", utcPtr, ":00"
+        do freqPtr = 1, 8
+            write(ASC_FILE, '(AF6.3A)') "Freq:", freqs(freqPtr), "MHz"
+            write(ASC_FILE, '(A3, A8, 2A10, A5, 23A8)') "id", "gcdkm", "Latitude", "Longitude", "Mode", &
+                    "MUF", "FOT", "ANGLE", "DELAY", "VHITE", "MUFday", "LOSS", "DBU", "SDBW", "NDBW", "SNR", &
+                    "RPWRG", "REL", "MPROB", "SPRB", "SIGLW", "SIGUP", "SNRLW", "SNRUP", "TGAIN", "RGAIN",  &
+                    "SNRxx", "DBM"
+            do ptr = NUMDIST-1, 0, -1
+                read(DST_FILE, rec=((utcPtr-1)*HOURBLK)+(ptr*8)+freqPtr ) gcdkm,xlat,xlon,xmode, MUF, &
                     FOT, ANGLE, DELAY, VHITE, MUFday, LOSS, DBU, SDBW, &
                     NDBW, SNR, RPWRG, REL, MPROB, SPRB, SIGLW, SIGUP, &
                     SNRLW, SNRUP, TGAIN, RGAIN, SNRxx, DBM
-                write(30, '(I3, F8.1, 2F10.4, A5, 23F8.3)') NUMDIST-ptr, gcdkm, xlat, xlon, xmode, MUF, FOT, ANGLE, &
+                write(ASC_FILE, '(I3, F8.1, 2F10.4, A5, 23F8.3)') NUMDIST-ptr, gcdkm, xlat, xlon, xmode, MUF, FOT, ANGLE, &
                     DELAY, VHITE, MUFday, LOSS, DBU, SDBW, NDBW, SNR, RPWRG, REL, MPROB, SPRB, SIGLW, SIGUP, &
                     SNRLW, SNRUP, TGAIN, RGAIN, SNRxx, DBM
-            END DO
-        END DO
-    END DO
+            end do
+        end do
+    end do
+    close(DST_FILE)
+    close(ASC_FILE)
 end program dst2ascii
--- voacapl/itshfbc/bin/dst/dst2csv.f90
+++ voacapl/itshfbc/bin/dst/dst2csv.f90
@@ -4,31 +4,30 @@
 !
 program dst2csv
     logical*1 file_exists
-    REAL :: gcdkm, xlat, xlon, MUF, FOT, ANGLE, DELAY, VHITE, MUFday, LOSS
-    REAL :: DBU, SDBW, NDBW, SNR, RPWRG, REL, MPROB, SPRB, SIGLW, SIGUP, SNRLW, SNRUP
-    REAL :: TGAIN, RGAIN, SNRxx, DBM
-    INTEGER :: num_args
-    CHARACTER(len=128) :: itshfbc_path = ""
-    INTEGER :: ios
-    INTEGER :: NUMDIST
-    INTEGER :: NUMFREQ
-    INTEGER :: NUMHOUR
-    REAL, DIMENSION(1 : 20) :: FREQS
-    INTEGER, PARAMETER :: IDX_FILE = 70
-    INTEGER, PARAMETER :: DST_FILE = 80
-    INTEGER, PARAMETER :: CSV_FILE = 90
-    INTEGER :: HOURBLK
-    INTEGER :: ptr, utcPtr, freqPtr, offset, id = 0
-    CHARACTER(200) :: index_buffer
-    CHARACTER(4) :: xmode
-    CHARACTER(120) :: RUN_DIR
-    CHARACTER(len=1), parameter :: PATH_SEPARATOR ='/'
-    CHARACTER(len=128) :: idx_path, dst_path
+    real :: gcdkm, xlat, xlon, MUF, FOT, ANGLE, DELAY, VHITE, MUFday, LOSS
+    real :: DBU, SDBW, NDBW, SNR, RPWRG, REL, MPROB, SPRB, SIGLW, SIGUP, SNRLW, SNRUP
+    real :: TGAIN, RGAIN, SNRxx, DBM
+    integer :: num_args
+    character(len=128) :: itshfbc_path = ""
+    integer :: ios
+    integer :: NUMDIST, NUMFREQ, NUMHOUR
+    real, dimension(1 : 25) :: FREQS
+    integer, dimension(1 : 25) :: hours
+    integer, parameter :: IDX_FILE = 70
+    integer, parameter :: DST_FILE = 80
+    integer, parameter :: CSV_FILE = 90
+    integer :: HOURBLK
+    integer :: ptr, utcPtr, freqPtr, offset, id = 0
+    character(200) :: index_buffer
+    character(4) :: xmode
+    character(120) :: RUN_DIR
+    character(len=1), parameter :: PATH_SEPARATOR ='/'
+    character(len=128) :: idx_path, dst_path
 
     num_args = command_argument_count()
     
     if (num_args == 1) then
-        CALL get_command_argument(1, itshfbc_path)
+        call get_command_argument(1, itshfbc_path)
         idx_path = trim(itshfbc_path)//PATH_SEPARATOR//'run'//PATH_SEPARATOR//'voacapd.idx'
         dst_path = trim(itshfbc_path)//PATH_SEPARATOR//'run'//PATH_SEPARATOR//'voacapd.dst'
     else
@@ -37,52 +36,58 @@ program dst2csv
     end if
     
     inquire(file=idx_path, exist=file_exists)
-    if (.NOT.file_exists) then
+    if (.not.file_exists) then
         stop 'Unable to open IDX file '//idx_path
     end if
 
     inquire(file=dst_path, exist=file_exists)
-    if (.NOT.file_exists) then
+    if (.not.file_exists) then
         stop 'Unable to open DST file '//idx_path
     end if
 
-    !print *, "Opening IDX file"
-    Open(IDX_FILE, file=idx_path, status='old')
+    open(IDX_FILE, file=idx_path, status='old')
+    ! Read the number of distances
     read(IDX_FILE, '(I5A)') NUMDIST, index_buffer
-    !print *, NUMDIST
-    read(IDX_FILE, '(I3A)') NUMFREQ, index_buffer
-    !print *, NUMFREQ
 
-    DO freqPtr = 1, NUMFREQ
+    ! Read the frequencies
+    read(IDX_FILE, '(I2A)') NUMFREQ, index_buffer
+    do freqPtr = 1, NUMFREQ
         offset = 1 + (7*(freqPtr-1))
         read(index_buffer((offset): (offset+7)), '(F7.3)') FREQS(freqPtr)
-        !print *, FREQS(freqPtr)
-    END DO
-    
-    HOURBLK = NUMDIST * NUMFREQ
+    end do
+
+    ! Read the hours
+    read(IDX_FILE, '(I3A)') NUMHOUR, index_buffer
+    do ptr = 1, NUMHOUR
+        offset = 1 + (3*(ptr-1))
+        read(index_buffer((offset): (offset+3)), '(I3)') hours(ptr)
+    end do
+
     close(IDX_FILE)
-    !print *, "Opening DST file"
+
+    HOURBLK = NUMDIST * NUMFREQ
+
     open(DST_FILE,file=dst_path,status='old', form='unformatted',access='direct',recl=108)
     open(CSV_FILE,file='voacapd.csv')
     rewind(CSV_FILE)
     write(CSV_FILE, '(A)') "id,utc,chan,freq,gcdkm,Latitude,Longitude,Mode,MUF,FOT,ANGLE,DELAY,VHITE,MUFday,LOSS, &
         DBU,SDBW,NDBW,SNR,RPWRG,REL,MPROB,SPRB,SIGLW,SIGUP,SNRLW,SNRUP,TGAIN,RGAIN,SNRxx,DBM"
 
-    DO utcPtr = 1, 24
-        DO freqPtr = 1, NUMFREQ
-            DO ptr = NUMDIST-1, 0, -1
+    do utcPtr = 1, NUMHOUR
+        do freqPtr = 1, NUMFREQ
+            do ptr = NUMDIST-1, 0, -1
                 id = id + 1
                 read(DST_FILE, rec=((utcPtr-1)*HOURBLK)+(ptr*8)+freqPtr ) gcdkm,xlat,xlon,xmode, MUF, &
                     FOT, ANGLE, DELAY, VHITE, MUFday, LOSS, DBU, SDBW, &
                     NDBW, SNR, RPWRG, REL, MPROB, SPRB, SIGLW, SIGUP, &
                     SNRLW, SNRUP, TGAIN, RGAIN, SNRxx, DBM
                 write(CSV_FILE, '(3(I0,","), F0.3, A, F0.1, A, 2(F0.4,","), A4, 23(",",F0.3))') &
-                    id, utcPtr, freqPtr, FREQS(freqPtr), ",", gcdkm, ",", xlat, xlon, xmode, MUF, FOT, ANGLE, &
+                    id, hours(utcPtr), freqPtr, FREQS(freqPtr), ",", gcdkm, ",", xlat, xlon, xmode, MUF, FOT, ANGLE, &
                     DELAY, VHITE, MUFday, LOSS, DBU, SDBW, NDBW, SNR, RPWRG, REL, MPROB, SPRB, SIGLW, SIGUP, &
                     SNRLW, SNRUP, TGAIN, RGAIN, SNRxx, DBM
-            END DO
-        END DO
-    END DO
+            end do
+        end do
+    end do
     close(DST_FILE)
     close(CSV_FILE)
 end program dst2csv
