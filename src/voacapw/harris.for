b204192b79153dc1ee324a4a32f2fc754d60f1a2
James Watson <jwatson@neomailbox.ch>
2018-02-27 19:35:37 +0300

    Fixes a typo in which the space between the anttyp90 exe and the first arg had been deleted.

--- src/voacapw/harris.for
+++ src/voacapw/harris.for
@@ -32,12 +32,12 @@ C
 C**********Harris modification M Packer for Area Coverage 11 May 1998 *******
 C
 c        This handles the Harris Type 99 antenna files.  Call the external
-c        routines (written in C) to prepare the 
+c        routines (written in C) to prepare the
 c        GAINxx.DAT for area coverage.  Then read in the file.
 c        We're gonna use numbers 250-299 if necessary.
 
 c           Follow the same routine necessary for AREA type 13 antenna files.
-         
+
 c           The difficulty here is that antcalc is writing the GAINxx.DAT
 c           file on lu 22 and we want to call a routine to generate the
 c           GAINxx.DAT file.
@@ -45,7 +45,7 @@ c           GAINxx.DAT file.
 c           Prepare ANTTYPxx.DAT to communicate with ANTTYPxx.EXE:
       open( 99, file=run_directory(1:nch_run)//PATH_SEPARATOR//dat,err=900)
       write( 99, 289 ) idx, antfile, fs,fe,beam_main,offazim
-289   format( i2, 28x,   '=antenna number (idx)' / 
+289   format( i2, 28x,   '=antenna number (idx)' /
      2       a21,  9x,  '=antenna file filename' /
      3      f7.3, 23x, '=antenna starting frequency (MHz)' /
      4      f7.3, 23x, '=antenna ending frequency (MHz)' /
@@ -70,11 +70,11 @@ c jw      if(iexe_exist.ne.1) then    !  antenna program does not exist
       end if
 c**********************************************************************
 C        Call ANTTYPxx.EXE to create GAINxx.DAT file:
-C                
+C
       PROGRAM=run_directory(1:nch_run-3)//'bin_win'//PATH_SEPARATOR//exe//' '//
      +             run_directory(1:nch_run)//' '//mode
 #else
-      PROGRAM=exe//run_directory(1:nch_run)//' '//mode
+      PROGRAM=exe//' '//run_directory(1:nch_run)//' '//mode
 #endif
       nch=lcount(PROGRAM,300)
 c jw      call gh_exec(PROGRAM,nch,1)   !  execute and wait for ANTTYPxx.EXE
@@ -84,7 +84,7 @@ c jw      call gh_exec(PROGRAM,nch,1)   !  execute and wait for ANTTYPxx.EXE
 C           Open GAINxx.DAT file
       open(22,file=run_directory(1:nch_run)//PATH_SEPARATOR//fileant,
      1            status='old', err=910)
-      rewind(22)  
+      rewind(22)
       read(22,'(a)') alf
       read(22,'(a)') alf
       read(22, 292 ) freq, eff
@@ -97,12 +97,12 @@ c jw         call yieldit
          read( 22, 293 ) ( areagain(ielev), ielev=1,91 )
 293      format( 9x, 10f7.3 )
          call antsave( idx, iazim, freq, eff, areagain )
-      end do  
+      end do
 
 c           We are done.  Close up, take your ball, and go home.
-      close( 22 ) 
+      close( 22 )
       return
-c*****End Harris mod      *************************************************   
+c*****End Harris mod      *************************************************
 900   write(*,901) run_directory(1:nch_run)//dat
 901   format('Could not create file:',a)
       go to 920
