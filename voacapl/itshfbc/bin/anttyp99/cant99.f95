24477b0804108bccc5e461b69b1e9ab43f71a630
James Watson <jimwatson@mac.com>
2018-03-05 18:36:49 +0300

    onverted to f95

--- voacapl/itshfbc/bin/anttyp99/cant99.f95
+++ voacapl/itshfbc/bin/anttyp99/cant99.f95
@@ -1,7 +1,7 @@
 module Cant99
     implicit none
     integer :: luaa, nfreq, itype
-    real :: ifreq1, ifreq2
+    integer :: ifreq1, ifreq2
     real, dimension(20) :: parms
     real, dimension(100) :: frequency,dbi,eff
     real, dimension(91, 360) :: gain1, gain2
