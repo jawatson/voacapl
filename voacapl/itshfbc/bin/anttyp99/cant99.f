4f99594e63e311d4b183859bc2d25f65125a1cc3
James Watson <jimwatson@mac.com>
2018-03-05 09:01:21 +0300

    Module of the Common Antenna 99 stuff

---
+++ voacapl/itshfbc/bin/anttyp99/cant99.f
@@ -0,0 +1,9 @@
+       module Cant99
+       integer :: luaa, nfreq, itype
+       real :: ifreq1, ifreq2
+       real, dimension(20) :: parms
+       real, dimension(100) :: frequency,dbi,eff
+       real, dimension(91, 360) :: gain1, gain2
+       character(len=80) :: filenam,title
+       end module
+
