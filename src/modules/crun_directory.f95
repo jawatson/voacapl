5f4484ca144de774d982417e98fdcf32f288bd8a
James Watson <jwatson@neomailbox.ch>
2018-03-09 05:32:11 +0300

    Adds the modules

---
+++ src/modules/crun_directory.f95
@@ -0,0 +1,5 @@
+module crun_directory
+    implicit none
+    character(len=128) :: root_directory
+    character(len=128) :: run_directory
+end module crun_directory
---
+++ src/modules/cversn.f95
@@ -0,0 +1,4 @@
+module cversn
+    implicit none
+    character(len=8) :: versn
+end module cversn
